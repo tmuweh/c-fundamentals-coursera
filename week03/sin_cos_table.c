726eec3592db6082fc6355de98f1cf6e7c535a6f
Tangue Muweh <atmuweh@gmail.com>
2020-01-09 20:05:40 +0100

    added a function to do the printing of the table called print_table()

--- week03/sin_cos_table.c
+++ week03/sin_cos_table.c
@@ -8,22 +8,37 @@
 #include <stdio.h>
 #include <math.h> // gets cos() and sin() functions here
 
-#define MIN 0
-#define MAX 1
+void print_table(int values); // to print a stable for sine and cosine
 
 int main(void)
 {
+  int values;
+
+  printf("%s", "Enter of Number of values to print on the table:");
+  scanf("%d", &values);
+  while (values < 2) { // aleast two values should be availabel use to calculate sin and cos of
+    printf("%s", "Enter of Number of values to print on the table:");
+    scanf("%d\n", &values);
+  }
+
+  print_table(values);
+
+  return 0;
+}
+
+// function definition
+void print_table(int values) {
+  /* code */
   double interval;
   int i;
 
   printf("X\t\t|SIN(X)\t\t|COSINE(X)\n");
-  for(i = 0; i < 25; i++)
+  for(i = 0; i < values; i++)
   {
     printf("_________________________________________\n");
 
-    interval = (i == 0 ? (i + 0.02) : i) / 25.0; // ensures that inverval shouldn't start from 0
+    interval = (i == 0 ? (i + 0.02) : i) / (values + 0.0); // ensures that inverval shouldn't start from 0
     printf("%.3lf\t\t|%.3lf\t\t|%.3lf\n", interval, sin(interval), cos(interval) );
   }
   printf("_________________________________________\n");
-  return 0;
 }
