b40242371c82c3ba9be2937aca550f78384c6477
Tangue Muweh <atmuweh@gmail.com>
2020-01-21 17:03:31 +0100

    print grades and corresponding location in memory

--- week05/pointers.c
+++ week05/pointers.c
@@ -7,11 +7,15 @@
 #define SIZE 10
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  int grades[SIZE];
+  int grades[SIZE] = {1, 2, 3, 4, 5, 6};
   int *ptr;  //pointer that points to an address in memorry
   ptr = grades;
-  printf("%d\t%d\n", *ptr, *grades);
+  printf("Grades\tlim\n");
+  for(int i = 0; i < SIZE; i++)
+    printf("%d\t%ld\n", ptr[i], (long)(ptr + (i * sizeof(long)))); //printing grades and address in memory
+  printf("*lim = location in memory\n" );
   return 0;
 }
