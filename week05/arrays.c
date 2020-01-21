c4fe0604690c69b4fea1656a63fd131735c8e47b
Tangue Muweh <atmuweh@gmail.com>
2020-01-21 02:36:24 +0100

    declared an array of fixed size

---
+++ week05/arrays.c
@@ -0,0 +1,18 @@
+/*
+  Tangue Muweh
+  Arrays
+  void1.0
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+#ifndef SIZE
+#define  SIZE 100
+#endif
+
+int main(void) {
+    int num[SIZE];
+    
+  return 0;
+}
