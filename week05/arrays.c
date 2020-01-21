24638e41274c6327b84d289e85ab166825df21f4
Tangue Muweh <atmuweh@gmail.com>
2020-01-21 03:32:15 +0100

    re--initialized all array alements to 1 using loop. curly bracets initialized rest to zero

--- week05/arrays.c
+++ week05/arrays.c
@@ -7,12 +7,19 @@
 #include <stdio.h>
 #include <math.h>
 
-#ifndef SIZE
-#define  SIZE 100
-#endif
 
 int main(void) {
-    int num[100] = {1}; //initialize all arrays with one
-    
+    int num[10];
+    int i;
+    double sum = 0.0;
+
+    for (i = 0; i < 10; i++) {
+
+       num[i] = 1; //initialize all arrays with one
+    }
+    for (i = 0; i < 10; i++) {
+
+       printf("%d\n",num[i]); //initialize all arrays with one
+    }
   return 0;
 }
