a0f5fdbddb15e58fdb6fc044405142f82e10516e
Tangue Muweh <atmuweh@gmail.com>
2020-01-26 03:35:10 +0100

    read first 50 numbers f in file and display

--- week05/files.c
+++ week05/files.c
@@ -13,6 +13,18 @@ int main(void){
 	//Perform operation on file
 	else{
 		printf("Perform operations with file.\n");
+		int i = 0;
+		int counter = 0; //counts number of values in file to allocate size of array to contain!
+		while(!feof(fp)){
+			counter++;
+			fscanf(fp, "%d", &data_array[i]);
+			i++;
+		}
+
+		printf("number of elephant_seal_data: %d\n", counter);
+		for(i = 0; i < 50; i++){
+			printf("%d\n", data_array[i] );
+		}
 
 	}
 
