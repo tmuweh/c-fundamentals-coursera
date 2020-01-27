3acea78c729f169780068a7e27fc934e707cb726
Tangue Muweh <atmuweh@gmail.com>
2020-01-27 01:05:03 +0100

    create a function to handle the average weight calculation. addes a similar files.c file with in complete solution to the average elephant weight problem

--- week05/elephant_seal_data.c
+++ week05/elephant_seal_data.c
@@ -7,11 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int average_elephant_weight(int n , int array[]);
+
 int main(void){
 
 	FILE *fp = fopen("elephant_seal_data.txt", "r");
 	int elephant_raw_data[10000]={0}; //initialize array to 0
-	int average_weight = 0;
 	
 	//check if file was succesfully opened for reading
 	if(fp == NULL){
@@ -27,12 +29,20 @@ int main(void){
 	}
 
 
-	printf("number of elephant_raw_data: %d\n", counter - 1);
-	for(int i = 0; i < counter-1; i++)
-		//calculate average weight of elephants
-		average_weight += (elephant_raw_data[i] - average_weight)/(i + 1); //i + 1 because indexing starts from zero
-	printf("average_elephant_seal_weight: %d\n", average_weight );
+	printf("\nNumber of elephant_raw_data: %d\n\n", counter - 1);
+	printf("average_elephant_seal_weight: %d\n\n", average_elephant_weight(counter, elephant_raw_data) );
+	
 	fclose(fp);
 
 	return 0;
+}
+
+
+int average_elephant_weight(int n, int array[]){
+	int average_weight = 0;
+	for(int i = 0; i < n-1; i++)
+		//calculate average weight of elephants
+		average_weight += (array[i] - average_weight)/(i + 1); //i + 1 because indexing starts from zero
+
+	return average_weight;
 }
--- week05/files.c
+++ week05/files.c
@@ -14,22 +14,21 @@ int main(void){
 	//Perform operation on file
 	fseek(fp, 0, SEEK_END); //seek to end of file
 	int SIZE = ftell(fp); //get size of file
-	printf("file size: %d", SIZE);
-	int data_array[SIZE]; //set size of file to size of array
-	fseek(fp, 0, SEEK_SET ); // Seek back to begining of file
-	SIZE = ftell(fp);
-	printf("file size: %d", SIZE);
+	printf("file size: %d", SIZE/4);
+	int data_array[SIZE/4]; //set size of file to size of array
+	rewind(fp);
+	printf("file size: %d", SIZE/4);
 	printf("Perform operations with file.\n");
 	int i = 0;
 	int counter = 0; //counts number of values in file to allocate size of array to contain!
-	while(i < SIZE){
+	while(i < SIZE/4){
 		counter++;
 		fscanf(fp, "%d", &data_array[i]);
 		i++;
 	}
 
 	printf("number of elephant_seal_data: %d\n", counter);
-	for(i = 0; i < SIZE; i++){
+	for(i = 0; i < SIZE/4; i++){
 		printf("%d\n", data_array[i] );
 	}
 
